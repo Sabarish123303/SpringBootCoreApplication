63e0b76ba05ceb0377cb6accbc4a4973ebb6a2d3
Sabarish123303 <seetharaman2699@gmail.com>
2022-05-16 20:01:58 +0530

    added show data service

--- src/main/java/com/csi/controller/ApplicationController.java
+++ src/main/java/com/csi/controller/ApplicationController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ApplicationController {
@@ -21,5 +24,51 @@ public class ApplicationController {
     public String address()
     {
         return "India";
+    }
+    @GetMapping("/showdata")
+    public List<Employee> showData()
+    {
+        List<Employee> empList = new LinkedList<>();
+        empList.add(new Employee(121,"RADHA"));
+        empList.add(new Employee(122,"RANI"));
+        empList.add(new Employee(124,"SWATI"));
+        empList.add(new Employee(125,"SWADESHI"));
+
+        return empList;
+    }
+
+}
+class Employee
+{
+    private int empId;
+    private String empName;
+
+    public Employee(int empId, String empName) {
+        this.empId = empId;
+        this.empName = empName;
+    }
+
+    public Employee(int empId) {
+        this.empId = empId;
+    }
+
+    public Employee(String empName) {
+        this.empName = empName;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
     }
 }
